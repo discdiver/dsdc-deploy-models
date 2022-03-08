8df30e2cfa9389181109bd9d75543bcbe06ed051
Jeff Hale <jeffmshale@gmail.com>
2022-03-07 23:10:51 -0500

    improve flask json

--- flask/flask_json.py
+++ flask/flask_json.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 
+app = Flask(__name__)
+
 
 @app.route("/likes.json")
 def json_data():
@@ -11,3 +13,10 @@ def json_data():
         "Height": 6,
     }
     return jsonify(favs), 200  # use jsonify function, required in flask
+
+
+# To run, in the terminal:
+# export FLASK_APP=flask_hello
+# flask run
+# can share on the network with flask run --host=0.0.0.0
+# to enable debugging - dont use in prod! export FLASK_ENV=development
